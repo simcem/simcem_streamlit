c820fc61b3a1e34f2dde10200768f0345a3bf1ad
Marcus Bannerman <m.bannerman@gmail.com>
2026-01-31 22:50:33 +0000

    Got a working derivation for streamlit, need to package pycalphad next

--- flake.nix
+++ flake.nix
@@ -14,29 +14,33 @@
           #overlays = [ self.overlays.default ];
         };
 
-  postShellHook = ''
-    PYTHONPATH=\$PWD/\${venvDir}/\${pkgs.python3.sitePackages}/:\$PYTHONPATH
-    # pip install -r requirements.txt
-  '';
+        simcem = (import (pkgs.fetchgit {
+          url="https://github.com/simcem/SimCem.git"; 
+          rev="1c0371748c0fbec816e29f18fe91d6bc2fb60840";
+          hash="sha256-LS+1RjkXMHTK+XhdAxkuPd/aeFeWCU+UuipDAcW+5rs=";
+          fetchSubmodules=true;
+          # Because we need submodules, we need to workaround it not being deterministic
+          # See https://github.com/NixOS/nixpkgs/issues/100498#issuecomment-1846499310
+          leaveDotGit = true;
+          postFetch = ''
+            rm -rf $out/.git
+          '';
+        }) { inherit pkgs; });
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            bashInteractive
-            git
-            screen
             (python3.withPackages (ps: with ps; [
               uv
               pip
-              pybind11
-              venvShellHook
+              simcem.simcem
+              pandas
+              pint
+              plotly
+              streamlit
+              scipy
             ]))
-            ipopt
-            eigen
-            boost
-            sundials
           ];
-          postShellHook = postShellHook;
         };
       })  
   ;
